445d203533cc91332cda0760a72c663271f669e2
Robert Vari <mail.pythonsuli@gmail.com>
2021-08-21 15:55:15 +0200

    update

--- Modules/movie_details.py
+++ Modules/movie_details.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import QObject, Slot
+from PySide6.QtCore import QObject, Slot, Property
 import tmdbsimple as tmdb
 from dotenv import load_dotenv
 import os
@@ -18,6 +18,22 @@ tmdb.API_KEY = os.getenv("TMDB_API_KEY")
 
 
 class MovieDetails(QObject):
+
     @Slot(str)
     def set_movie(self, movie_id):
-        print(f"Get details for movie: {movie_id}")
+        movie = tmdb.Movies(movie_id)
+        response = movie.info()
+
+        if not response:
+            return
+
+    title = Property(str, _get_title, notify=movie_loaded)
+    overview = Property(str, _get_overview, notify=movie_loaded)
+    tagline = Property(str, _get_tagline, notify=movie_loaded)
+    release_date = Property(str, _get_date, notify=movie_loaded)
+    poster = Property(QUrl, _get_poster, notify=movie_loaded)
+    backdrop = Property(QUrl, _get_backdrop, notify=movie_loaded)
+    language = Property(str, _get_language, notify=movie_loaded)
+    genres = Property(str, _get_genres, notify=movie_loaded)
+    runtime = Property(str, _get_runtime, notify=movie_loaded)
+    vote_average = Property(int, _get_vote_average, notify=movie_loaded)
